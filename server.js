4014c07dec281f04c21e129c5fac3bd43d9c3d1c
KortoVos <larshofschlaeger@gmail.com>
2017-06-05 01:05:01 +0200

    tasdt

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
   fs = require('fs'),
   request = require('request'),
   cheerio = require('cheerio'),
-  app     = express();
-//var analyse = require('./analyse.js');
-//var allSerials = require('./allSerials.js');
+  app     = express(),
+  analyse = require('./analyse.js'),
+  allSerials = require('./allSerials.js');
 
 //var MongoClient = require('mongodb').MongoClient;
 
@@ -15,6 +15,25 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80,
     mongoURLLabel = "";
 
 
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
 
 /*
 app.get('/scrape', function(req, res){
@@ -52,7 +71,7 @@ app.get('/getNewSerials', function(req, res){
 */
 
 app.get('/', function (req, res) {
-  res.send('4 Real never?');
+  res.send(mongoURLLabel + " - " + mongoURL);
 });
 
 app.get('/pagecount', function (req, res) {
