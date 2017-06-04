afa4b34ba3e1d6cc160c1192b2528ad3e93c6864
KortoVos <larshofschlaeger@gmail.com>
2017-06-04 23:35:11 +0200

    dfsaads

--- analyse.js
+++ analyse.js
@@ -19,7 +19,7 @@ function getSerial(serial){
 	        serial.title 		= 	data.children().first().text().match('\\t(.*?)\\n')[0].trim(); 
 	        serial.description =data.children().next().children().html();  
 	        serial.genres 	= 	data.children().next().next().children().children().next().html().split('</span>').map(obj=>obj.split('>')[1]).filter((obj)=>obj != null); 
-	        let release = data.children().next().next().children().next().html().match('<em>(.*?) - (.*?)</').splice(1,2);
+	        var release = data.children().next().next().children().next().html().match('<em>(.*?) - (.*?)</').splice(1,2);
 	        serial.release  = {"start":release[0],"end":release[1]}
 	        serial.actors 	= 	data.children().next().next().children().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
 	        serial.producer = 	data.children().next().next().children().next().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
