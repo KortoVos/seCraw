67eeb50d35cd6b39f3b9b0140cd2f9fde1b0f6bb
KortoVos <larshofschlaeger@gmail.com>
2017-06-05 01:49:14 +0200

    back to work

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
   fs = require('fs'),
   request = require('request'),
   cheerio = require('cheerio'),
-  app     = express()/*,
-  analyse = require('./analyse.js'),
-  allSerials = require('./allSerials.js')*/;
+  app     = express();
+//var analyse = require('./analyse.js');
+//var allSerials = require('./allSerials.js');
 
 //var MongoClient = require('mongodb').MongoClient;
 
@@ -15,25 +15,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80,
     mongoURLLabel = "";
 
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
 
 /*
 app.get('/scrape', function(req, res){
@@ -71,8 +52,7 @@ app.get('/getNewSerials', function(req, res){
 */
 
 app.get('/', function (req, res) {
-  res.send("mongo test");
-  console.log('Mongo is running on http://%s:%s', mongoURL, mongoURLLabel);
+  res.send('4 Real never?');
 });
 
 app.get('/pagecount', function (req, res) {
@@ -85,7 +65,7 @@ app.use(function(err, req, res, next){
 });
 
 var server = app.listen(port, ip, function () {
-  console.log('Server is running on http://%s:%s', ip, port);
+  console.log('Server running on http://%s:%s', ip, port);
 });
 
 
