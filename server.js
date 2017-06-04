b273ac17a9cfddf3136cdbb77accaad8d9fbdbb7
KortoVos <larshofschlaeger@gmail.com>
2017-06-04 23:18:21 +0200

    openshift try

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var analyse = require('./analyse.js');
+var allSerials = require('./allSerials.js');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+
+
+/*
+app.get('/scrape', function(req, res){
+  res.send("startet");
+  search();
+})
+
+function search(){
+    analyse.getSerial(data.rows[0].value).then(function(result){
+        console.log("saved!");
+        //search()
+
+    }, err => {
+      console.log("error while searching!");
+    });
+}
+
+app.get('/getNewSerials', function(req, res){
+  url = 'https://bs.to/andere-serien';
+  
+  allSerials.getAllSerials(url).then(function(result){
+    //fs.writeFile('test.json', JSON.stringify(result, null, 4));
+    res.json(result);
+    result.map((el,i) => { 
+      setTimeout(function() {
+        console.log(el );
+        couch.uniqid().then(function(ids){
+          couch.insert(dbName,el)
+        });
+      },(Math.random()*10000))
+    });
+
+  });
+})
+*/
+
+var server = app.listen(port, ip, function () {
+  console.log('Server running on http://%s:%s', ip, port);
+});
