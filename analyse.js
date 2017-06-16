c44af1f49452a6295b960a74f7f1cad0dc268b88
KortoVos <larshofschlaeger@gmail.com>
2017-06-16 20:08:45 +0200

    asdf

--- analyse.js
+++ analyse.js
@@ -13,20 +13,14 @@ function getSerial(serial){
 			result = null;
 			//global.gc();
 			//console.log("Page loaded");
-			serial.title 		= 	data.children().first().text().match('\\t(.*?)\\n')[0].trim(); 
+			serial.latestCheck=	Date.now();
+	        serial.title 	  =data.children().first().text().match('\\t(.*?)\\n')[0].trim(); 
 	        serial.description =data.children().next().children().html();  
 	        serial.genres 	= 	data.children().next().next().children().children().next().html().split('</span>').map(obj=>obj.split('>')[1]).filter((obj)=>obj != null); 
-	        var release = data.children().next().next().children().next().html().match('<em>(.*?) - (.*?)</').splice(1,2);
-	        serial.release  = {"start":release[0],"end":release[1]}
-	        serial.actors 	= 	data.children().next().next().children().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
-	        serial.producer = 	data.children().next().next().children().next().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
-	        serial.director = 	data.children().next().next().children().next().next().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
-	        serial.author 	=   data.children().next().next().children().next().next().next().next().next().children().next().html().split('>').filter(o=>o.match(("(.*?),</span|</span"))).map(o=>o.split(',')[0]).map(o=>o.split('<')[0]);
-		    });
-		    $('#sp_right').filter(function(){ 
-		        var data = $(this);
-		        serial.img = data.children().first().attr("src");
-	        });
+	        
+	        //var release = data.children().next().next().children().next().html().match('<em>(.*?) - (.*?)</').splice(1,2);
+	        serial.release  = {"start":"2004","end":"2009"}
+
 			resolve(serial);
 		});
 
